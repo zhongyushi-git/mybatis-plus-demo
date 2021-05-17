9121ba3c1ebcc028e4b5a53b2639547b65ac79e7
zhongyushi <1916008067@qq.com>
2021-05-17 15:16:44 +0800

    添加配置类

---
+++ src/main/java/com/zys/mybatisplusdemo/config/MyMetaObjectHandler.java
@@ -0,0 +1,21 @@
+package com.zys.mybatisplusdemo.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Date;
+
+@Configuration
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("createTime", new Date(), metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+}
